db7a77de5d24a9985af3991cad93f7885b155ff9
Jui <cse_1912020087@gmail.com>
2024-05-05 23:40:27 +0600

    access_modifier

---
+++ access_modifier.ts
@@ -0,0 +1,38 @@
+
+{
+  //
+
+// Access modifiers
+
+class BankAccount{
+public readonly id:number;
+public name:string;
+protected _balance :number;
+
+  constructor(id:number,name:string,balance:number){
+    this.id=id;
+    this.name=name;
+    this._balance =balance;
+  }
+  addDeposit(amount :number){
+
+    this._balance =this._balance+amount
+
+  }
+  getBalance(){
+    return this._balance;
+  }
+}
+class StudentAccount extends BankAccount{
+  test(){
+    
+  }
+}
+const herAccount = new BankAccount(111, 'Mrs.Jui',40);
+herAccount.name='jui'
+herAccount.addDeposit(0);
+const myBalance = herAccount.getBalance();
+console.log(myBalance);
+
+//
+}
