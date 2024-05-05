9637d562fa0e3defdf8a11541648baa37e5814d4
Jui <cse_1912020087@gmail.com>
2024-05-05 16:17:56 +0600

    instanceof  Guard

--- guard.ts
+++ guard.ts
@@ -48,5 +48,59 @@
 getUser(normalUser);
 
 
+// instance of guard
+class Animal{
+  name:string;
+  species:string;
+
+  constructor(name:string,species:string){
+    this.name=name;
+    this.species = species;
+  }
+  makeSound(){
+    console.log("I am making sound");
+  }
+}
+
+class Dog extends Animal{
+  constructor(name:string,species:string){
+    super(name,species);
+  }
+  makeBark(){
+    console.log("I am barking");
+  }
+}
+
+class Cat extends Animal{
+  constructor(name:string,species:string){
+    super(name,species);
+  }
+  makeMewa(){
+    console.log("I am Mewaing");
+  }
+}
+// check using function with instanceof:
+const isDog=(animal:Animal): animal is Dog=>{
+  return animal instanceof Dog;
+}
+const isCat=(animal:Animal): animal is Cat=>{
+return animal instanceof Cat;
+}
+const getAnimal =(animal:Animal)=>{
+  if(isDog(animal)){
+    animal.makeBark();
+  }
+  else if(isCat(animal)){
+    animal.makeMewa()
+  }
+  else{
+    animal.makeSound();
+  }
+}
+
+const dog =new Dog('Dog vai','dog');
+const cat =new Cat('cat vai','cat');
+
+getAnimal(dog);
   // 
 }
